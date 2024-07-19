637700c9838d10b8829d4ba44f9b970cafd1ed5e
raees <mohitdamke1@gmail.com>
2024-07-19 20:55:15 +0530

    Done With Login & Register & Logout in thread

---
+++ app/src/main/java/com/example/threadpractice/viewmodel/AuthViewModel.kt
@@ -0,0 +1,152 @@
+package com.example.threadpractice.viewmodel
+
+import android.content.Context
+import android.net.Uri
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.threadpractice.model.UserModel
+import com.example.threadpractice.util.SharedPref
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import java.util.UUID
+
+class AuthViewModel : ViewModel() {
+
+    val auth = FirebaseAuth.getInstance()
+    val currentUser = auth.currentUser
+
+    private val db = FirebaseDatabase.getInstance()
+    val userRef = db.getReference("users")
+
+    private val storageRef = Firebase.storage.reference
+    val imageRef = storageRef.child("users/${UUID.randomUUID()}.jpg")
+
+    private val _firebaseUser = MutableLiveData<FirebaseUser?>()
+    val firebaseUser: LiveData<FirebaseUser?> = _firebaseUser
+
+    private val _error = MutableLiveData<String>()
+    val error: LiveData<String> = _error
+
+    init {
+        _firebaseUser.value = currentUser
+    }
+
+    fun login(email: String, password: String, context: Context) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    _firebaseUser.postValue(auth.currentUser)
+                    getData(auth.currentUser!!.uid, context)
+                    _error.postValue("You have successfully login")
+                } else {
+                _error.postValue( task.exception?.message)
+                }
+            }
+    }
+
+    private fun getData(uid: String, context: Context) {
+        userRef.child(uid).addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val userData = snapshot.getValue(UserModel::class.java)
+                SharedPref.storeData(
+                    userData!!.name,
+                    userData.userName,
+                    userData.email,
+                    userData.bio,
+                    userData.imageUrl,
+                    context
+                )
+            }
+
+            override fun onCancelled(error: DatabaseError) {}
+        })
+    }
+
+    fun register(email: String, password: String, name: String, bio: String, userName: String, imageUri: Uri, context: Context) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    _firebaseUser.postValue(firebaseUser.value)
+                    saveImage(
+                        email,
+                        password,
+                        name,
+                        bio,
+                        userName,
+                        imageUri,
+                        auth.currentUser?.uid,
+                        context
+                    )
+                } else {
+                    _error.value = "Register failed. Please try again."
+                }
+            }
+    }
+
+    private fun saveImage(
+        email: String,
+        password: String,
+        name: String,
+        bio: String,
+        userName: String,
+        imageUri: Uri?,
+        uid: String?,
+        context: Context
+    ) {
+
+        val uploadTask = imageRef.putFile(imageUri!!)
+        uploadTask.addOnSuccessListener {
+            imageRef.downloadUrl.addOnSuccessListener { uri ->
+                saveData(email, password, name, bio, userName, uri.toString(), uid, context)
+            }
+        }
+
+
+    }
+
+    private fun saveData(
+        email: String,
+        password: String,
+        name: String,
+        bio: String,
+        userName: String,
+        toString: String,
+        uid: String?,
+        context: Context
+    ) {
+
+        val userData = UserModel(email, password, name, bio, userName, toString, uid!!)
+
+        userRef.child(uid!!).setValue(userData)
+            .addOnSuccessListener {
+                SharedPref.storeData(
+                    name, userName, email, bio, toString, context
+                )
+            }.addOnFailureListener {}
+
+
+    }
+
+    fun logout() {
+        auth.signOut()
+        _firebaseUser.postValue(null)
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
